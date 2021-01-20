68b2909236696c6a8f70e0ef930b8f43ed3f907a
trevor-r-allen <tallen2699@gmail.com>
2021-01-19 17:59:32 -0700

    samll change

--- src/components/ApodDisplayComponent.vue
+++ src/components/ApodDisplayComponent.vue
@@ -1,17 +1,32 @@
 <template>
-  <img
-    :src="apod.url"
-    alt=""
-  >
+  <div class="card">
+    <img
+      class="card-img-top"
+      :src="apod.url"
+      alt=""
+    >
+    <div class="card-body">
+      <h4 class="card-title">
+        {{ apod.title }}
+      </h4>
+      <p class="card-text">
+        {{ apod.explanation }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
 import { AppState } from '../AppState'
 export default {
   name: 'ApodDisplayComponent',
   setup () {
+    const state = reactive({
+
+    })
     return {
+      state,
       apod: computed(() => AppState.apod)
     }
   }
